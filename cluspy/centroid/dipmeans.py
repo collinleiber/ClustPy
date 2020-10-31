fix diptest precision bug

cluspy/centroid/dipmeans.py
@@ -28,10 +28,8 @@ def _dipmeans(X, pval_threshold, split_viewers_threshold, pval_strategy, n_boots
             ids_in_each_cluster.append(ids_in_cluster)
             # Get pairwise distances of points in cluster
             cluster_dist_matrix = data_dist_matrix[np.ix_(ids_in_cluster, ids_in_cluster)]
-            # Sort distances
-            sorted_cluster_dist_matrix = np.sort(cluster_dist_matrix, axis=1)
             # Calculate dip values for the distances of each point
-            cluster_dips = np.array([dip(sorted_cluster_dist_matrix[p, :], just_dip=True, data_is_sorted=True) for p in
+            cluster_dips = np.array([dip(cluster_dist_matrix[p, :], just_dip=True) for p in
                                      range(ids_in_cluster.shape[0])])
             # Calculate p-values
             if pval_strategy == PVAL_BY_BOOT:

cluspy/centroid/projected_dipmeans.py
@@ -25,11 +25,9 @@ def _proj_dipmeans(X, pval_threshold, n_random_projections, pval_strategy, n_boo
             ids_in_each_cluster.append(ids_in_cluster)
             # Get projections
             projections = _get_projected_data(X[ids_in_cluster], n_random_projections)
-            # Sort distances
-            sorted_projections = np.sort(projections, axis=0)
             # Calculate dip values for the distances of each point
-            cluster_dips = np.array([dip(sorted_projections[:, p], just_dip=True, data_is_sorted=True) for p in
-                                     range(sorted_projections.shape[1])])
+            cluster_dips = np.array([dip(projections[:, p], just_dip=True) for p in
+                                     range(projections.shape[1])])
             # Calculate p-values of maximum dip
             pval = dip_pval(np.max(cluster_dips), ids_in_cluster.shape[0], pval_strategy=pval_strategy, n_boots=n_boots)
             # Calculate cluster score
@@ -49,21 +47,17 @@ def _proj_dipmeans(X, pval_threshold, n_random_projections, pval_strategy, n_boo
 
 
 def _get_projected_data(X, n_random_projections):
-    n_projections = 2 * X.shape[1] + n_random_projections  # Axis + PCA + random
-    projections = np.zeros((X.shape[0], n_projections))
     # Execute PCA
-    pca = PCA(n_components=X.shape[1])
-    pca_X = pca.fit_transform(X)
-    for i in range(X.shape[1]):
-        # Add axis vector
-        projections[:, i * 2] = X[:, i]
-        # Add pca vector
-        projections[:, i * 2 + 1] = pca_X[:, i]
+    pca = PCA()
+    pca_X = pca.fit_transform(X) if X.shape[0] > 1 else np.empty((X.shape[0], 0))
+    # Add random projections
+    random_projections = np.zeros((X.shape[0], n_random_projections))
     for i in range(n_random_projections):
         # Add random vector
         projection_vector = np.random.rand(X.shape[1])
         projected_X = np.dot(X, projection_vector)
-        projections[:, 2 * X.shape[1] + i] = projected_X
+        random_projections[:, i] = projected_X
+    projections = np.c_[X, pca_X, random_projections]
     return projections
 
 

cluspy/statistics/diptest.py
@@ -21,17 +21,15 @@ PVAL_BY_BOOT = 1
 PVAL_BY_FUNCTION = 2
 
 
-def dip(X, just_dip=False, data_is_sorted=False):
+def dip(X, just_dip=False):
     """
         Compute the Hartigans' dip statistic either for a histogram of
         samples (with equidistant bins) or for a set of samples.
     """
-    if not data_is_sorted:
-        X = np.sort(X)
-
-    counts = collections.Counter(X)
-    idxs = np.msort(list(counts.keys()))
-    histogram = np.array([counts[i] for i in idxs])
+    # Set precision to less than float64 since the subtraction in _lcm_ is having problems with the precision and
+    # produces duplicates
+    X = np.around(X, 15)
+    idxs, histogram = np.unique(X, return_counts=True)
 
     # check for case 1<N<4 or all identical values
     if len(idxs) <= 4 or idxs[0] == idxs[-1]:
@@ -92,7 +90,7 @@ def dip(X, just_dip=False, data_is_sorted=False):
         right[:0] = right_part[xr:-1]
 
 
-def diptest(X, data_is_sorted=False, pval_strategy=PVAL_BY_TABLE, n_boots=2000):
+def diptest(X, pval_strategy=PVAL_BY_TABLE, n_boots=2000):
     """
     Hartigan & Hartigan's dip test for unimodality.
     For X ~ F i.i.d., the null hypothesis is that F is a unimodal distribution.
@@ -119,7 +117,7 @@ def diptest(X, data_is_sorted=False, pval_strategy=PVAL_BY_TABLE, n_boots=2000):
         The Annals of Statistics.
     """
     n_points = X.shape[0]
-    data_dip = dip(X, just_dip=True, data_is_sorted=data_is_sorted)
+    data_dip = dip(X, just_dip=True)
     pval = dip_pval(data_dip, n_points, pval_strategy, n_boots)
     return data_dip, pval
 
@@ -139,17 +137,19 @@ def dip_pval(data_dip, n_points, pval_strategy=PVAL_BY_TABLE, n_boots=2000):
             "pval_strategy must be 0 (table), 1 (boot) or 2 (function). Your input: {0}".format(pval_strategy))
     return pval
 
+
 def dip_boot_samples(n_points, n_boots):
     # random uniform vectors
     boot_samples = np.random.rand(n_boots, n_points)
     # faster to pre-sort
-    boot_samples.sort(axis=1)
-    boot_dips = np.array([dip(boot_s, just_dip=True, data_is_sorted=True) for boot_s in boot_samples])
+    boot_dips = np.array([dip(boot_s, just_dip=True) for boot_s in boot_samples])
     return boot_dips
+
 
 """
 Dip p-value methods
 """
+
 
 def _dip_pval_table(data_dip, n_points):
     N, SIG, CV = _dip_table_values()
@@ -168,6 +168,7 @@ def _dip_pval_table(data_dip, n_points):
     sD = np.sqrt(n_points) * data_dip
     pval = 1. - np.interp(sD, y0 + fn * (y1 - y0), SIG)
     return pval
+
 
 def _dip_pval_function(data_dip, n_points):
     a = 1.0