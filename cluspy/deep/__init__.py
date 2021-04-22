22f9addf387821dbed1e931c350132b6abc5f294
lgmb92 <77614713+lgmb92@users.noreply.github.com>
2021-04-22 12:42:56 +0200

    Update __init__.py
    Stacked_Autoencoder importiert

--- cluspy/deep/__init__.py
+++ cluspy/deep/__init__.py
@@ -2,11 +2,12 @@ from .dec import DEC, IDEC
 from .dcn import DCN
 from .vade import VaDE
 from .dipdeck import DipDECK
-from ._utils import Simple_Autoencoder
+from ._utils import Simple_Autoencoder, Stacked_Autoencoder
 
 __all__ = ['DEC',
            'IDEC',
            'DCN',
            'VaDE',
            'DipDECK',
-           'Simple_Autoencoder']
+           'Simple_Autoencoder',
+           'Stacked_Autoencoder']
