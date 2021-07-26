bf3b946f573f89b9bbeeeece4d863767e317c444
Fridolin Sack <fridolin.sack@web.de>
2021-07-26 13:27:37 +0200

    new method to create synthetic nr data

--- cluspy/data/__init__.py
+++ cluspy/data/__init__.py
@@ -1,10 +1,11 @@
-from .synthetic_data_creator import create_subspace_data
+from .synthetic_data_creator import create_subspace_data, create_nr_data
 from .real_world_data import load_har, load_usps, load_mnist, load_fmnist, load_kmnist, load_letterrecognition, \
     load_optdigits, load_pendigits, load_newsgroups, load_iris, load_wine, load_breast_cancer, load_reuters, \
     load_banknotes, load_htru2, load_motestrain, load_cmu_faces, load_mice_protein, load_webkb, load_ecoli, load_spambase, \
     load_seeds
 
 __all__ = ['create_subspace_data',
+           'create_nr_data',
            'load_har',
            'load_usps',
            'load_mnist',
