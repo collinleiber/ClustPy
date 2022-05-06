900ba63066720576d4471e9381d9350b91e34d2e
lukas <lukas.miklautz@univie.ac.at>
2022-05-06 17:19:09 +0200

    Included comments from review of @collinleiber #20

--- cluspy/deep/flexible_autoencoder.py
+++ cluspy/deep/flexible_autoencoder.py
@@ -49,16 +49,17 @@ class FullyConnectedBlock(nn.Module):
         return self.block(x)
 
 class FlexibleAutoencoder(torch.nn.Module):
-    """A feedforward symmetric autoencoder.
+    """A feedforward autoencoder.
     
     Parameters
     ----------
     layers : list of the different layer sizes from input to embedding, e.g. an example architecture for MNIST [784, 512, 256, 10], where 784 is the input dimension and 10 the embedding dimension. 
-             The decoder is symmetric and goes in the same order from embedding to input.
+             If decoder_layers are not specified then the decoder is symmetric and goes in the same order from embedding to input.
     batch_norm : bool, default=False, set True if you want to use torch.nn.BatchNorm1d
     dropout : float, default=None, set the amount of dropout you want to use.
     activation: activation function from torch.nn, default=torch.nn.LeakyReLU, set the activation function for the hidden layers, if None then it will be linear. 
     bias : bool, default=True, set False if you do not want to use a bias term in the linear layers
+    decoder_layers : list, default=None, list of different layer sizes from embedding to output of the decoder. If set to None, will be symmetric to layers.
     decoder_output_fn : activation function from torch.nn, default=None, set the activation function for the decoder output layer, if None then it will be linear. 
                         e.g. set to torch.nn.Sigmoid if you want to scale the decoder output between 0 and 1. 
     Attributes
@@ -66,7 +67,7 @@ class FlexibleAutoencoder(torch.nn.Module):
     encoder : encoder part of the autoencoder, responsible for embedding data points
     decoder : decoder part of the autoencoder, responsible for reconstructing data points from the embedding
     """
-    def __init__(self, layers, batch_norm=False, dropout=None, activation_fn=torch.nn.LeakyReLU, bias=True, decoder_output_fn=None):
+    def __init__(self, layers, batch_norm=False, dropout=None, activation_fn=torch.nn.LeakyReLU, bias=True, decoder_layers=None, decoder_output_fn=None):
         super(FlexibleAutoencoder, self).__init__()
         self.layers = layers
         self.batch_norm = batch_norm
@@ -74,12 +75,21 @@ class FlexibleAutoencoder(torch.nn.Module):
         self.activation_fn = activation_fn
         self.bias = bias
         self.decoder_output_fn = decoder_output_fn
+        if decoder_layers is None:
+            self.decoder_layers = self.layers[::-1]
+        else:
+            self.decoder_layers = decoder_layers
+        if (self.layers[-1] != self.decoder_layers[0]):
+            raise ValueError(f"Innermost hidden layer and first decoder layer do not match, they are {self.layers[-1]} and {self.decoder_layers[0]} respectively.")
+        if (self.layers[0] != self.decoder_layers[-1]):
+            raise ValueError(f"Output and input dimension do not match, they are {self.layers[0]} and {self.decoder_layers[-1]} respectively.")
+
         
         # Initialize encoder
         self.encoder = FullyConnectedBlock(layers=self.layers, batch_norm=self.batch_norm, dropout=self.dropout, activation_fn=self.activation_fn, bias=self.bias, output_fn=None)
         
         # Inverts the list of layers to make symmetric version of the encoder
-        self.decoder = FullyConnectedBlock(layers=self.layers[::-1], batch_norm=self.batch_norm, dropout=self.dropout, activation_fn=self.activation_fn, bias=self.bias, output_fn=self.decoder_output_fn)
+        self.decoder = FullyConnectedBlock(layers=self.decoder_layers, batch_norm=self.batch_norm, dropout=self.dropout, activation_fn=self.activation_fn, bias=self.bias, output_fn=self.decoder_output_fn)
 
     
     def encode(self, x:torch.Tensor)->torch.Tensor:
@@ -145,7 +155,7 @@ class FlexibleAutoencoder(torch.nn.Module):
             loss /= len(dataloader)
         return loss
 
-    def fit(self, n_epochs, lr, batch_size=128, data=None, data_eval=None, dataloader=None, evalloader=None, optimizer_fn=torch.optim.Adam, loss_fn=torch.nn.MSELoss(), patience=5, lr_reduction_factor=0.5, device=torch.device("cpu"), model_path=None, print_step=10):
+    def fit(self, n_epochs, lr, batch_size=128, data=None, data_eval=None, dataloader=None, evalloader=None, optimizer_fn=torch.optim.Adam, loss_fn=torch.nn.MSELoss(), patience=5, scheduler=None, scheduler_params=None, device=torch.device("cpu"), model_path=None, print_step=10):
         """Trains the autoencoder in place.
         
         Parameters
@@ -156,11 +166,13 @@ class FlexibleAutoencoder(torch.nn.Module):
         data : np.ndarray, default=None, train data set. If data is passed then dataloader can remain empty
         data_eval : np.ndarray, default=None, evaluation data set. If data_eval is passed then evalloader can remain empty.
         dataloader : torch.utils.data.DataLoader, default=None, dataloader to be used for training
-        evalloader : torch.utils.data.DataLoader, default=None, dataloader to be used for evaluation, early stopping and learning rate scheduling
+        evalloader : torch.utils.data.DataLoader, default=None, dataloader to be used for evaluation, early stopping and learning rate scheduling if scheduler=torch.optim.lr_scheduler.ReduceLROnPlateau
         optimizer_fn : torch.optim, default=torch.optim.Adam, optimizer to be used
         loss_fn : torch.nn, default=torch.nn.MSELoss(), loss function to be used for reconstruction
-        patience : int, default=5, patience parameter for learning rate scheduler and early stopping
-        lr_reduction_factor : float, default=0.5, factor for reducing the learning rate if loss plateu is reached, if None or 0 than learning rate scheduler is not used. Based on torch.optim.lr_scheduler.ReduceLROnPlateau.
+        patience : int, default=5, patience parameter for learning rate scheduler
+        scheduler : torch.optim.lr_scheduler, default=None, learning rate scheduler that should be used. 
+                    If torch.optim.lr_scheduler.ReduceLROnPlateau is used then the behaviour is matched by providing the validation_loss calculated based on samples from evalloader.
+        scheduler_params : dict, default=None, dictionary of the parameters of the scheduler object
         device : torch.device, default=torch.device('cpu'), device to be trained on
         model_path : str, default=None, if specified will save the trained model to the location. If evalloader is used, then only the best model w.r.t. evaluation loss is saved.
         print_step : int, default=10, specifies how often the losses are printed
@@ -168,6 +180,7 @@ class FlexibleAutoencoder(torch.nn.Module):
         Raises
         ----------
         ValueError: data cannot be None if dataloader is None
+        ValueError: evalloader cannot be None if scheduler=torch.optim.lr_scheduler.ReduceLROnPlateau
         """
         if dataloader is None:
             if data is None:
@@ -175,7 +188,7 @@ class FlexibleAutoencoder(torch.nn.Module):
             dataloader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(torch.from_numpy(data).float()),
                                                     batch_size=batch_size,
                                                     shuffle=True)
-        # evalloader has priority
+        # evalloader has priority over data_eval
         if evalloader is None:
             if data_eval is not None:
                 evalloader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(torch.from_numpy(data_eval).float()),
@@ -186,10 +199,15 @@ class FlexibleAutoencoder(torch.nn.Module):
         optimizer = optimizer_fn(**params_dict)
         
         early_stopping = EarlyStopping(patience=patience)
-        if (lr_reduction_factor is not None) and (lr_reduction_factor > 0):
-            scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor=lr_reduction_factor, patience=patience, verbose=True)
-        else:
-            scheduler = None
+        if scheduler is not None:
+            scheduler = scheduler(optimizer=optimizer, **scheduler_params)
+            # Depending on the scheduler type we need a different step function call.
+            if isinstance(scheduler, torch.optim.lr_scheduler.ReduceLROnPlateau):
+                eval_step_scheduler = True
+                if evalloader is None:
+                    raise ValueError("scheduler=torch.optim.lr_scheduler.ReduceLROnPlateau, but evalloader is None. Specify evalloader such that validation loss can be computed.")
+            else:
+                eval_step_scheduler = False
         best_loss = np.inf
         i = 0
         # training loop
@@ -202,15 +220,18 @@ class FlexibleAutoencoder(torch.nn.Module):
                 optimizer.zero_grad()
                 loss.backward()
                 optimizer.step()
-            if (epoch_i-1) % print_step == 0 or epoch_i == n_epochs:
-                print(f"Epoch {epoch_i-1}/{n_epochs} - Batch Reconstruction loss: {loss.item():.6f}")
+            if (epoch_i-1) % print_step == 0 or epoch_i == (n_epochs-1):
+                print(f"Epoch {epoch_i}/{n_epochs-1} - Batch Reconstruction loss: {loss.item():.6f}")
             
+            
+            if scheduler is not None and not eval_step_scheduler:
+                scheduler.step()
             # Evaluate autoencoder
             if evalloader is not None:
-                self.eval()
+                # self.evaluate calls self.eval()
                 val_loss = self.evaluate(dataloader=evalloader, loss_fn=loss_fn, device=device)
                 if (epoch_i-1) % print_step == 0 or epoch_i == n_epochs:
-                    print(f"Epoch {epoch_i-1} EVAL loss total: {val_loss.item():.6f}")
+                    print(f"Epoch {epoch_i} EVAL loss total: {val_loss.item():.6f}")
                 early_stopping(val_loss)
                 if val_loss < best_loss:
                     best_loss = val_loss
@@ -220,10 +241,10 @@ class FlexibleAutoencoder(torch.nn.Module):
                         torch.save(self.state_dict(), model_path)
 
                 if early_stopping.early_stop:
-                    print(f"Stop training at epoch {best_epoch-1}")
+                    print(f"Stop training at epoch {best_epoch}")
                     print(f"Best Loss: {best_loss:.6f}, Last Loss: {val_loss:.6f}")
                     break
-                if scheduler is not None:
+                if scheduler is not None and eval_step_scheduler:
                     scheduler.step(val_loss)
         # Save last version of model
         if evalloader is None:
