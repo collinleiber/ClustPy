79c7853138905b8b88ecf4e3549b00e0ccfea43b
Collin Leiber <leiber@dbs.ifi.lmu.de>
2023-03-16 15:04:46 +0100

    try to use project version automatically for documentation

--- docs/conf.py
+++ docs/conf.py
@@ -12,6 +12,7 @@
 
 import os
 import sys
+import clustpy
 sys.path.insert(0, os.path.abspath('.'))
 
 
@@ -22,7 +23,7 @@ copyright = '2023, Collin Leiber'
 author = 'Collin Leiber'
 
 # The full version, including alpha/beta/rc tags
-release = 'v0.0.1-alpha'
+release = clustpy.__version__
 
 
 # -- General configuration ---------------------------------------------------
