1e508ff1273921c64ed9cfcd37a81a9c30c2f44b
lukas <lukas.miklautz@univie.ac.at>
2023-06-29 16:29:52 +0200

    fixed linting problem in resnet_ae_modules.py

--- clustpy/deep/autoencoders/resnet_ae_modules.py
+++ clustpy/deep/autoencoders/resnet_ae_modules.py
@@ -323,9 +323,9 @@ class ResNetDecoder(nn.Module):
 
 
 
-__all__ = ['ResNet', 'resnet18', 'resnet34', 'resnet50', 'resnet101',
-           'resnet152', 'resnext50_32x4d', 'resnext101_32x8d',
-           'wide_resnet50_2', 'wide_resnet101_2']
+# __all__ = ['ResNet', 'resnet18', 'resnet34', 'resnet50', 'resnet101',
+#            'resnet152', 'resnext50_32x4d', 'resnext101_32x8d',
+#            'wide_resnet50_2', 'wide_resnet101_2']
 
 
 model_urls = {
