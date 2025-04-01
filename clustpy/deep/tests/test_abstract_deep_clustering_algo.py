251ab25ccb2af42e89bfdb4776f0296d2fbdefc7
Collin Leiber <collin.leiber@aalto.fi>
2025-04-01 22:41:46 +0300

    add missing line to abstract deep clustering test

--- clustpy/deep/tests/test_abstract_deep_clustering_algo.py
+++ clustpy/deep/tests/test_abstract_deep_clustering_algo.py
@@ -9,6 +9,7 @@ def test_abstract_deep_clustering_algo():
     adca = _AbstractDeepClusteringAlgo(256, fw_ae, None, 10, torch.device("cpu"), 1)
     adca.neural_network_trained_ = adca.neural_network
     adca.labels_ = None
+    adca.n_features_in_ = 53
     X, _ = create_subspace_data(1500, subspace_features=(3, 50), random_state=1)
     X_embed = adca.transform(X)
     assert X_embed.shape == (1500, 4)
